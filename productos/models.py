ab61eada477747b15d99b2cd46fa199a50ff0ee8
eberesquivel <eberesquivel@gmail.com>
2020-06-30 18:27:40 -0500

    cambios

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Cerveza(models.Model):
+    marca = models.CharField(max_length=50)
+    alcohol = models.DecimalField(max_digits=4, decimal_places=2)
+    mililitros = models.IntegerField()
+    artesanal = models.BooleanField()
+    nacionalidad = models.CharField(max_length=50,blank=True,null=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    editado = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.marca
