c5f90c5b360a0dab95d5f0acad1ad0cc5986f23c
eberesquivel <eberesquivel@gmail.com>
2020-06-30 19:50:07 -0500

    Se utiliza la clase serializada en un ViewSet

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets 
+from .models import Cerveza
+from .serializers import CervezaSerializer
 # Create your views here.
+
+class ChelaViewSet(viewsets.ModelViewSet):
+    serializer_class = CervezaSerializer
+    queryset = Cerveza.objects.all()
