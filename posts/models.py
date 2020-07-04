7c8a7246892a41b214ddd5060b599ab777051906
eberesquivel <eberesquivel@gmail.com>
2020-07-04 14:12:56 -0500

    modelo posts creado

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length= 300)
+    creado = models.DateTimeField(auto_now_add=True)
+    editado = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.marca
