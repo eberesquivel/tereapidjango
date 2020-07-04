7fed948b56da69d0224d1f8659e639db46ee808f
eberesquivel <eberesquivel@gmail.com>
2020-07-04 14:21:58 -0500

    desplegando modelo en Django REST Frame

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets 
+from .models import Posts
+from .serializers import PostSerializer
 # Create your views here.
+
+class PostsViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Posts.objects.all()
