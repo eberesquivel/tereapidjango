a037b92036f787cf847154c4e65fbcd95158c095
eberesquivel <eberesquivel@gmail.com>
2020-07-04 14:42:18 -0500

    agregando postViewSet a urls

--- tereapi/urls.py
+++ tereapi/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from productos.views import CervezaViewSet
+from posts.views import PostsViewSet
 
 router = DefaultRouter()
 router.register(r'productos',CervezaViewSet)
-
+router.register(r'posts',PostsViewSet)
 urlpatterns = router.urls
 
 urlpatterns += [
